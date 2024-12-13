a21a1d795292af2089e45b69c31412aee9ebea40
liuzhe <113238986+ConsoleLZ@users.noreply.github.com>
2024-12-13 09:51:21 +0800

    Update index-DaKQjPer.js
    fix

--- assets/index-DaKQjPer.js
+++ assets/index-DaKQjPer.js
@@ -1,1 +1,1 @@
-import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c,o}from"./index-CcAUJD_t.js";const r={};function n(t,s){return o(),c("div",null," 404 ")}const f=e(r,[["render",n]]);export{f as default};
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c,o}from"./index-CcAUJD_t.js";const r={};function n(t,s){return o(),c("div",null," 404 ")}const f=e(r,[["render",n]]);export{f as default};
