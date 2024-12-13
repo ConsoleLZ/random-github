4c139a2f8c44eb2c539cb3a5956c47229c371383
liuzhe01 <liuzhe01@mucang.cn>
2024-12-13 11:25:18 +0800

    fix:修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: '/random-github/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		plugins: [vue()],
+		base: env.VITE_BASE_URL || '/',
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, 'src')
+			}
+		}
+	};
+});
